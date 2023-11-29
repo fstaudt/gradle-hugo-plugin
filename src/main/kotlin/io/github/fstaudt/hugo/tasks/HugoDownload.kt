88c83901380923d2d86e3dcc0789e3871d8c4a47
Paul Merlin <paul@nosphere.org>
2023-11-30 00:55:10 +0100

    fix: make hugo tasks compatible with configuration cache

--- src/main/kotlin/io/github/fstaudt/hugo/tasks/HugoDownload.kt
+++ src/main/kotlin/io/github/fstaudt/hugo/tasks/HugoDownload.kt
@@ -2,6 +2,9 @@ package io.github.fstaudt.hugo.tasks
 
 import io.github.fstaudt.hugo.HugoPluginExtension
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ArchiveOperations
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.file.ProjectLayout
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.OutputDirectory
@@ -10,9 +13,10 @@ import org.jetbrains.kotlin.gradle.internal.ensureParentDirsCreated
 import java.io.File
 import java.io.FileOutputStream
 import java.util.zip.ZipException
+import javax.inject.Inject
 
 @CacheableTask
-open class HugoDownload : DefaultTask() {
+abstract class HugoDownload : DefaultTask() {
 
     companion object {
         const val HUGO_DOWNLOAD = "hugoDownload"
@@ -24,24 +28,31 @@ open class HugoDownload : DefaultTask() {
     lateinit var extension: HugoPluginExtension
 
     @OutputDirectory
-    val output = File("${project.buildDir}/$BINARY_DIRECTORY")
+    val output = layout.buildDirectory.dir(BINARY_DIRECTORY).get().asFile
+
+    @get:Inject
+    protected abstract val layout: ProjectLayout
+
+    @get:Inject
+    protected abstract val fs: FileSystemOperations
+
+    @get:Inject
+    protected abstract val archives: ArchiveOperations
 
     @TaskAction
     fun download() {
-        with(project) {
-            downloadArchive().also {
-                when (it.extension) {
-                    "zip" -> copy { from(zipTree(it)); into(output) }
-                    "tgz", "tar", "gz" -> copy { from(tarTree(it)); into(output) }
-                    else -> throw ZipException("Unsupported extension for archive ${it.name}.")
-                }
+        downloadArchive().also {
+            when (it.extension) {
+                "zip" -> fs.copy { from(archives.zipTree(it)); into(output) }
+                "tgz", "tar", "gz" -> fs.copy { from(archives.tarTree(it)); into(output) }
+                else -> throw ZipException("Unsupported extension for archive ${it.name}.")
             }
         }
     }
 
     private fun downloadArchive(): File {
         val url = extension.downloadUrl()
-        return File("${project.buildDir}/$DOWNLOAD_DIRECTORY/${url.file}").also {
+        return layout.buildDirectory.file("$DOWNLOAD_DIRECTORY/${url.file}").get().asFile.also {
             it.ensureParentDirsCreated()
             logger.info("Downloading Hugo binary from $url")
             url.openStream().use { input ->
